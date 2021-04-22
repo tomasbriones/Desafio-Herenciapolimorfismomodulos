807914430c05d7e66ee721ea64db22cbb966d574
Tomas Briones <tbriones@economial.cl>
2021-04-21 21:25:01 -0400

    add ejercicio2

---
+++ ejercicio2.rb
@@ -0,0 +1,36 @@
+module Habilidades
+    module Volador
+        def volar
+            'Estoy volandooooo!'
+        end
+        def aterrizar
+        'Estoy cansado de volar, voy a aterrizar'
+        end
+    end
+    module Nadador
+        def nadar
+            'Estoy nadando!'
+        end
+        def sumergir
+            'glu glub glub glu'
+        end
+    end
+    module Caminante
+        def caminar
+            'Puedo caminar!'
+        end
+    end
+end
+
+module Alimentacion
+    module Herbivoro
+        def comer
+            'Puedo comer plantas!'
+        end
+    end
+    module Carnivoro
+        def comer
+            'Puedo comer carne!'
+        end
+    end
+end
